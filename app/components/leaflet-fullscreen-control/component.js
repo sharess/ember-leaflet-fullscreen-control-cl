221067d77c000e84600b5d61fa3e1dad4feb2986
Till Theato <till.theato@evocount.de>
2019-02-22 13:31:51 +0100

    Fix folder structure.

---
+++ app/components/leaflet-fullscreen-control/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-leaflet-fullscreen-control/components/leaflet-fullscreen-control/component';
--- app/leaflet-fullscreen-control/component.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-leaflet-fullscreen-control/leaflet-fullscreen-control/component';
